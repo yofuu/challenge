7972b89cb5288d948271186cae39347b04d38071
EC2 Default User <ec2-user@ip-172-31-25-109.ap-northeast-1.compute.internal>
2023-04-20 14:58:44 +0000

    challenge3

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'index' => 'javascript#index'
+  get 'app.js' => 'javascript#app.js'
 end
