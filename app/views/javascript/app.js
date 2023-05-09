a576c130686a0ab6850c08fec9cf49c349e1f4e1
EC2 Default User <ec2-user@ip-172-31-25-109.ap-northeast-1.compute.internal>
2023-05-09 12:06:07 +0000

    challenge10

--- app/views/javascript/app.js
+++ app/views/javascript/app.js
@@ -1,14 +1,15 @@
-// メイン部分
-let alertString;
+// // メイン部分
+// let alertString;
 
-// 作成した関数を呼び出し、変数へ格納
-alertString = addString("WebCamp");
+// // 作成した関数を呼び出し、変数へ格納
+// alertString = addString("WebCamp");
 
-//変数の中身をアラートで表示する
-alert(alertString);
+// //変数の中身をアラートで表示する
+// alert(alertString);
 
-// 作成した関数
-function addString(strA){
-  let addStr = "Hello " + strA;
-  return addStr;
-}
+// // 作成した関数
+// function addString(strA){
+//   let addStr = "Hello " + strA;
+//   return addStr;
+// }
+alert('Hello' + 'World');
